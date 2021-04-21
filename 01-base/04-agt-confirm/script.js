a583664054c7e2039f28719e0038e4ba651710ed
Laurent Bosteels <laurent.bosteels@gmail.com>
2021-04-21 17:40:23 +0200

    chapter one finished

---
+++ 01-base/04-agt-confirm/script.js
@@ -0,0 +1,27 @@
+/* becode/javascript
+ *
+ * /01-base/04-asv-confirm/script.js - 1.4: ASV avec confirmation
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function () {
+
+    let age;
+    let gender;
+    let town;
+    let answer = false;
+    while (answer == false) {
+        age = prompt('What is your age?');
+        gender = prompt('What is your gender?');
+        town = prompt('What is your town?');
+        if (confirm('Are you ' + age + ', are you ' + gender + ', and are you from ' + town + '? Is your info correct?')) {
+            answer = true;
+        }
+    }
+
+})();
