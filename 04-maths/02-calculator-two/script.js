bdb2781abf899b0439fa529ceda511b770fcc6b7
Laurent Bosteels <laurent.bosteels@gmail.com>
2021-04-22 17:15:18 +0200

    chapter four finished

---
+++ 04-maths/02-calculator-two/script.js
@@ -0,0 +1,48 @@
+/* becode/javascript
+ *
+ * /02-maths/02-calculator-two/script.js - 2.2: calculatrice (2)
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function () {
+    // to get the value of an input: document.getElementById("element-id").value
+
+    var performOperation = function (operation) {
+        // perform the operation
+
+        const opOne = parseInt(document.getElementById("op-one").value);
+        const opTwo = parseInt(document.getElementById("op-two").value);
+
+        switch (operation) {
+            case "addition": {
+                alert(opOne + opTwo);
+                break;
+            }
+
+            case "substraction": {
+                alert(opOne - opTwo);
+                break;
+
+            }
+            case "division": {
+                alert(opOne / opTwo);
+                break;
+            }
+
+            default: {
+                alert(opOne * opTwo);
+            }
+        }
+    };
+
+    Array.from(document.querySelectorAll("button.operator")).forEach(function ($btn) {
+        $btn.addEventListener("click", function () {
+            performOperation($btn.id);
+        });
+    });
+})();
