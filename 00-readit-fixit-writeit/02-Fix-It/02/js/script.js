8ba2645ecf96aa010253bb106ba8c365f3b44389
Laurent Bosteels <laurent.bosteels@gmail.com>
2021-04-23 15:56:02 +0200

    completed fit-it exercises

---
+++ 00-readit-fixit-writeit/02-Fix-It/02/js/script.js
@@ -0,0 +1,2 @@
+const amount = 10;
+document.write("<h1>The " + amount + " Most popular tv shows</h1>");
---
+++ 00-readit-fixit-writeit/02-Fix-It/05/js/script.js
@@ -0,0 +1,32 @@
+const shows = [
+  `legion`,
+  `sneaky pete`,
+  `santa clarita diet`,
+  `riverdale`,
+  `the young pope`,
+  `a series of unfortunate events`,
+  `taboo`,
+  `colony`,
+  `24: legacy`,
+  `speechless`,
+  `scherlock`,
+  `stranger things`,
+  `this is us`,
+  `timeless`,
+  `the oa`,
+];
+
+const wrapWithTag = (content, tagname) => `<${tagname}>${content}</${tagname}>`;
+
+const writeTitle = title => {
+  const newTitle = uCFirst(title);
+  document.write(wrapWithTag(newTitle, `li`));
+};
+
+const uCFirst = sentence => {
+return sentence.toLowerCase().split(' ').map(s => s.charAt(0).toUpperCase() + s.substring(1)).join(' ');
+};
+
+document.write(`<ol>`);
+shows.forEach(show => writeTitle(show));
+document.write(`</ol>`);
