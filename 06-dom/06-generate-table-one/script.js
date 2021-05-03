d885fe2c3da2beb40cd20f760901d6c2ccf0dd98
Laurent Bosteels <laurent.bosteels@gmail.com>
2021-05-03 15:38:18 +0200

    exercise 6 finished

---
+++ 06-dom/06-generate-table-one/script.js
@@ -0,0 +1,25 @@
+/* becode/javascript
+ *
+ * /06-dom/07-generate-table-one/script.js - 6.7: génération d'un tableau (1)
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+    const targetVar = document.getElementById("target");
+    const newTable = document.createElement("table");
+
+    for (let i = 0; i < 10; i++) {
+        let newRow = newTable.insertRow(i);
+        let cell1 = newRow.insertCell(0);
+
+        cell1.innerHTML = "test";
+        targetVar.appendChild(newTable);
+    }
+
+})();
