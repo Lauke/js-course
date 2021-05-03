b9fd81436d78961b61d8df8263045d24a914e125
Laurent Bosteels <laurent.bosteels@gmail.com>
2021-05-03 10:57:00 +0200

    exercise 04 finished

---
+++ 06-dom/04-attr-create/script.js
@@ -0,0 +1,24 @@
+/* becode/javascript
+ *
+ * /06-dom/04-attr-create/script.js - 6.4: manipulation d'attributs & création d'élément
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+/* Get the value of the "data-image" attribute from the element with id "source". Create a new image
+element inside the target element that displays the image referred to by the attribute value. Then remove
+the original element. */
+
+
+(function() {
+
+    const source = document.getElementById('source')
+    const img = document.createElement('img')
+    img.src = source.getAttribute('data-image');
+    target.appendChild(img);
+
+})();
