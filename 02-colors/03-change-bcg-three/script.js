1cbe10053506cad4a42af30f78f3160e90fcffb3
Laurent Bosteels <laurent.bosteels@gmail.com>
2021-05-05 00:28:25 +0200

    corrected exercise

--- 02-colors/03-change-bcg-three/script.js
+++ 02-colors/03-change-bcg-three/script.js
@@ -11,10 +11,9 @@
 
 (function () {
 
-    let randomColor
     document.getElementById('run').onclick = function changeBackground() {
-        randomColor = Math.floor(Math.random() * 16777215).toString(16);
-        document.body.style.backgroundColor = "#" + randomColor;
+        let randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+        document.body.style.backgroundColor = randomColor;
         console.log(randomColor);
     }
 
